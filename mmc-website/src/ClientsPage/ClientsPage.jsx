de58c4a48e9dea259a8504df16aa1fc6963fa9cb
kekons4 <kekons4@gmail.com>
2024-05-21 16:02:22 -0400

    added the layout for the contact us form

--- mmc-website/src/ClientsPage/ClientsPage.jsx
+++ mmc-website/src/ClientsPage/ClientsPage.jsx
@@ -8,7 +8,6 @@ function ClientsPage() {
     const classes = useStyles();
 
     const [dense, setDense] = useState(false);
-    const [secondary, setSecondary] = useState(false);
 
     return (
         <Container maxWidth="2xl" sx={{marginTop: "4rem", padding: "4rem", backgroundColor: "#efefef", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
@@ -16,41 +15,41 @@ function ClientsPage() {
                 <Typography variant='h2' component="div" align="center" sx={{fontFamily: "Cabin", marginBottom: "1rem"}}>
                     Our <span className={classes.consulting}>Clients</span>
                 </Typography>
-                <Typography variant='p' component="div" align='center' sx={{fontFamily: "Cabin", fontSize: {xs: "10px", sm: "15px", md: "20px"}}}>
+                <Typography variant='p' component="div" align='center' sx={{fontFamily: "Cabin", fontSize: {xs: "15px", sm: "20px", md: "25px"}}}>
                     We have worked with multiple companies in multiple industries. 
                 </Typography>
-                <Typography variant='p' component="div" align='center' sx={{fontFamily: "Cabin"}}>
+                <Typography variant='p' component="div" align='center' sx={{fontFamily: "Cabin", fontSize: {xs: "12px", sm: "15px", md: "20px"}}}>
                      <b>Automotive, Aerospace, Defense, Law Firms, and Materials</b>
                 </Typography>
             </Box>
             <Box>
                 <List dense={dense} sx={{border: "1px black solid", maxWidth: "700px"}}>
                     <ListItem>
-                        <img src="../images/kaiserLogo.jpeg" alt="General Motors Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/kaiserLogo.jpeg" alt="General Motors Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Kaiser Corp." secondary="(2024 - Present)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/gmLogo.png" alt="General Motors Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/gmLogo.png" alt="General Motors Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="GM Corp." secondary="(2021-2022)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/hondaLogo.png" alt="Honda Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/hondaLogo.png" alt="Honda Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Honda Corp." secondary="(2019-2021)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/hondaRD.jpeg" alt="Honda R&D America Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/hondaRD.jpeg" alt="Honda R&D America Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Honda R&D Americas, Inc." secondary="(2017-2019)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/ewiLogo.jpeg" alt="Edison Welding Institure Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/ewiLogo.jpeg" alt="Edison Welding Institure Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Edison Welding Institute" secondary="(2015-2017)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/varnum.png" alt="Varnum Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/varnum.png" alt="Varnum Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Varnum, Riddering, Schmidt, Howlett, LLP" secondary="(2004)" sx={{marginLeft: "20px"}}/>
                     </ListItem>
                     <ListItem>
-                        <img src="../images/varnum.png" alt="Varnum Logo" style={{height: "100px", width: "150px"}} />
+                        <img src="../images/vanDykeLogo.jpeg" alt="Varnum Logo" style={{height: "100px", width: "150px", borderRadius: "10px"}} />
                         <ListItemText primary="Van Dyke, Gardner, Linn & Burkhart, LLP" secondary="(2003)" sx={{marginLeft: "20px"}}/> 
                     </ListItem>
                 </List>
---
+++ mmc-website/src/ContactUs/ContactUs.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { Container, Typography, Box, TextField, MenuItem, InputLabel, FormControl, Select, Button } from "@mui/material";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import WorkIcon from '@mui/icons-material/Work';
+import MessageIcon from '@mui/icons-material/Message';
+import SendIcon from '@mui/icons-material/Send';
+import InputAdornment from '@mui/material/InputAdornment';
+
+// User Defined Modules
+import useStyles from "../utils/styles";
+
+function ContactUs() {
+    const classes = useStyles();
+
+    const [industry, setIndustry] = useState('');
+
+    const handleChange = (event) => {
+        setIndustry(event.target.value);
+    };
+
+    return (
+        <Container maxWidth="2xl" sx={{backgroundColor: "#efefef", marginTop: "2rem", padding: "4rem", display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <FormControl variant="standard" sx={{borderRadius: "10px", padding: "30px", backgroundColor: "white", display: "flex", flexDirection: "column", gap: "20px", boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"}}>
+                <Typography variant="h2" component="div" align="center" sx={{fontFamily: "Cabin"}}>
+                    Contact <span className={classes.consulting}>Us</span>
+                </Typography>
+                <Typography variant="p" component="div" align="center" sx={{fontFamily: "Cabin", fontSize: {xs: "12px", sm: "15px", md: "20px"}}}>
+                    If you have and business query please contact us entering your information in the form below. 
+                </Typography>
+                
+                <TextField 
+                    id="standard-basic" 
+                    label="Full Name" 
+                    variant="outlined"  
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <AccountCircle />
+                        </InputAdornment>
+                        ),
+                    }}
+                    />
+                <TextField 
+                    id="standard-basic" 
+                    label="Email" 
+                    variant="outlined"
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <MailOutlineIcon />
+                        </InputAdornment>
+                        ),
+                    }} 
+                    />
+                <TextField 
+                    id="standard-basic" 
+                    label="Company" 
+                    variant="outlined"
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <WorkIcon />
+                        </InputAdornment>
+                        ),
+                    }} 
+                    />
+                <FormControl variant="standard">
+                    <InputLabel id="industry-label">Industry</InputLabel>
+                    <Select
+                        labelId="industry-label"
+                        id="industry"
+                        value={industry}
+                        onChange={handleChange}
+                        autoWidth
+                        label="Industry"
+                        variant="outlined"
+                        >
+                            <MenuItem value="aerospace">Aerospace</MenuItem>
+                            <MenuItem value="automotive">Automotive</MenuItem>
+                            <MenuItem value="defense">Defense</MenuItem>
+                            <MenuItem value="law">Law</MenuItem>
+                            <MenuItem value="materials">Materials</MenuItem>
+                            <MenuItem value="other">Other</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField 
+                    multiline 
+                    id="message" 
+                    label="Message" 
+                    variant="outlined" 
+                    rows={6}
+                    />
+                <Button endIcon={<SendIcon />} variant="outlined" sx={{color: "#d32f2f", border: "1px #d32f2f solid", "&:hover": {color: "red", border: "1px red solid", backgroundColor: "#efefef", fontWeight: "900"}}}>Send</Button>
+            </FormControl>
+        </Container>
+    );
+}
+
+export default ContactUs;
